0ea5eb9f011f57bc297c341e3e49d977fc23044c
S M Priiiiyo Hossain <77372461+priiiiyo@users.noreply.github.com>
2021-06-03 22:04:53 +0000

    Moved some commands

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -221,13 +221,15 @@ if __name__ == "__main__":
     #
     upload_as_doc_handler = MessageHandler(
         upload_as_doc,
-        filters=filters.command(os.environ.get("TOGGLE_DOC", "toggledoc")+BOT) & filters.chat(chats=AUTH_CHANNEL)
+        filters=filters.command([f"{TOGGLE_DOC_COMMAND}"])
+        & filters.chat(chats=AUTH_CHANNEL),
     )
     app.add_handler(upload_as_doc_handler)
     #
     upload_as_video_handler = MessageHandler(
         upload_as_video,
-        filters=filters.command(os.environ.get("TOGGLE_VIDEO", "togglevideo")+BOT) & filters.chat(chats=AUTH_CHANNEL)
+        filters=filters.command([f"{TOGGLE_VIDEO_COMMAND}"])
+        & filters.chat(chats=AUTH_CHANNEL),
     )
     app.add_handler(upload_as_video_handler)
     clear_thumb_nail_handler = MessageHandler(
